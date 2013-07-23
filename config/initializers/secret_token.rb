c0e09b3e96ac3783d1384078c70bad4eb6e270b3
root <root@eso2.cityu.sk>
2013-07-23 15:25:58 +0200

    prvy commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '0db1dc53e435bdd6cd089b01c5b9bfec9f8d854d14d48c019e82fc24a3ae55296c991ee0d76a14f18ca955e98627b0b388e692d42c8ef69ca6d2d60719895554'
